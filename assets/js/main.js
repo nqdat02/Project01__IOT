7d69d2899a932b6fd1272b94c56cc42877d4054c
Nguyễn Quốc Đạt <134251073+nqdat02@users.noreply.github.com>
2024-01-20 12:17:07 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,160 @@
+// var data = [];
+// var lastDate = new Date('19 Jun 2017');
+// var options = {
+// 	series: [
+// 		{
+// 		data: data.slice(),
+// 		},
+// 	],
+
+// 	chart: {
+// 		id: "realtime",
+// 		height: 350,
+// 		type: "line",
+// 		animations: {
+// 			enabled: true,
+// 			easing: "linear",
+// 			dynamicAnimation: {
+// 				speed: 1000,
+// 			},
+// 		},
+
+// 		toolbar: {
+// 			show: false,
+// 		},
+
+// 		zoom: {
+// 			enabled: false,
+// 		},
+// 	},
+
+// 	dataLabels: {
+// 		enabled: false,
+// 	},
+
+// 	stroke: {
+// 		curve: "smooth",
+// 	},
+
+// 	title: {
+// 		text: "Dynamic Updating Chart",
+// 		align: "left",
+// 	},
+
+// 	markers: {
+// 		size: 0,
+// 	},
+
+// 	xaxis: {
+// 		type: "datetime",
+// 		// range: XAXISRANGE,
+// 		min: new Date('19 Jun 2017').getTime(),
+// 		max: new Date('14 Aug 2017').getTime(),
+// 	},
+
+// 	yaxis: {
+// 		max: 100,
+// 	},
+
+// 	legend: {
+// 		show: false,
+// 	},
+// };
+
+// var chart = new ApexCharts(document.querySelector("#myChart"), options);
+// chart.render();
+
+// window.setInterval(function () {
+// 	getNewSeries(lastDate, {
+// 		min: 10,
+// 		max: 90,
+// 	}
+// 	);
+
+// 	chart.updateSeries([
+// 		{
+// 		data: data,
+// 		},
+// 	]
+//   );
+// }, 1000);
+// function getNewSeries(baseval, yrange) {
+// 	var newDate = baseval + 2000;
+// 	lastDate = newDate;
+// 	data.push({
+// 		x: newDate,
+// 		y: Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min
+// 	});
+// }
+
+var data = {
+	labels: [0],
+	datasets: [
+		{
+		label: "Temparature",
+		data: [0],
+		borderColor: "rgb(189, 185, 199)",
+		lineTension: 0.5,
+		},
+	],
+};
+
+var config = {
+	type: "line",
+	data: data,
+};
+
+var myChart = new Chart(document.getElementById("myChart"), config);
+myChart.canvas.parentNode.style.height = '400px';
+myChart.canvas.parentNode.style.width = '1200px';
+
+window.setInterval(myCallback, 2000);
+
+function myCallback() {
+	var now = new Date();
+	var value = Math.floor(Math.random() * 100);
+	now = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+
+
+	if (data.labels.length > 15) {
+		data.labels.shift();
+		data.datasets[0].data.shift();
+	}
+	data.labels.push(now);
+	data.datasets[0].data.push(value);
+
+	myChart.update();
+
+	document.getElementById("currentValue").innerHTML = 'Current Temparature at ' + now + ' - ' + value;
+}
+
+var btn_light = document.querySelector('#btn-light .switch input');
+var btn_fan = document.querySelector('#btn-fan .switch input');
+
+btn_light.addEventListener('change', function() {
+	if (this.checked) {
+		console.log('Light is on');
+		document.querySelector('#btn-light p').innerHTML = 'ON';
+		document.getElementById('btn-light-off').style.display = 'none';
+		document.getElementById('btn-light-on').style.display = 'block';
+	} else {
+		console.log('Light is off');
+		document.querySelector('#btn-light p').innerHTML = 'OFF';
+		document.getElementById('btn-light-off').style.display = 'block';
+		document.getElementById('btn-light-on').style.display = 'none';	
+	}
+});
+
+btn_fan.addEventListener('change', function() {
+	if (this.checked) {
+		console.log('Fan is on');
+		document.querySelector('#btn-fan p').innerHTML = 'ON';
+		document.getElementById('btn-fan-off').style.display = 'none';
+		document.getElementById('btn-fan-on').style.display = 'block';
+	} else {
+		console.log('Fan is off');
+		document.querySelector('#btn-fan p').innerHTML = 'OFF';
+		document.getElementById('btn-fan-off').style.display = 'block';
+		document.getElementById('btn-fan-on').style.display = 'none';
+	}
+});
